f69ca7439467be9394cd13203a55ee9ebc2d315b
Justin Alan Ryan <serial.rockstar@gmail.com>
2013-03-24 09:48:11 -0700

    get the right kernel version, enable ssh server.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,41 @@
 node default {
-#  include "tor"
+  include "tmux"
+  include "tor"
   include "vlc"
   include "vagrant"
   include "spotify"
 
-  exec { "say-hello":
-    command => "echo 'HELLO PUPPET!'",
+  package { 'git': }
+
+  package {
+    'linux-image-3.2.0-39-generic':
+      ensure => present;
+    'linux-headers-3.2.0-39-generic':
+      ensure => present;
+    'linux-image-3.5.0-23-generic':
+      ensure => purged;
+    'linux-headers-3.5.0-23-generic':
+      ensure => purged;
+    'linux-image-3.5.0-26-generic':
+      ensure => purged;
+    'linux-headers-3.5.0-26-generic':
+      ensure => purged;
+    'linux-generic-lts-quantal':
+      ensure => purged;
+    'linux-image-generic-lts-quantal':
+      ensure => purged;
+    'linux-headers-lts-quantal':
+      ensure => purged;
+    'linux-image-lts-quantal':
+      ensure => purged;
+    'virtualbox':
+      ensure => present;
+    'openssh-server':
+      ensure => present;
   }
 
+  service { 'ssh':
+    ensure => running,
+    require => Package['openssh-server'],
+  }
 }
